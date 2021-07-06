f2766a1a9cd0711b378380468f844d2c4bf286cb
Youngjun Woo <youngjun_92@hotmail.com>
2021-07-06 00:52:18 -0700

    changed left, top to transform for better performance

--- coordinates/main.js
+++ coordinates/main.js
@@ -2,6 +2,10 @@ const vertical = document.querySelector('.vertical');
 const horizontal = document.querySelector('.horizontal');
 const target = document.querySelector('.target');
 const tag = document.querySelector('.tag');
+const targetRect = target.getBoundingClientRect();
+const targetHalfWidth = targetRect.width / 2;
+const targetHalfHeight = targetRect.height / 2;
+const tagRect = tag.getBoundingClientRect();
 
 // when mouse moves, move elements along with the mouse cursor
 document.addEventListener('mousemove', (event) => {
@@ -9,12 +13,14 @@ document.addEventListener('mousemove', (event) => {
   const y = event.clientY;
 
   // pass x, y coordinates to elements
-  vertical.style.left = `${x}px`;
-  horizontal.style.top = `${y}px`;
-  target.style.left = `${x}px`;
-  target.style.top = `${y}px`;
-  tag.style.left = `${x}px`;
-  tag.style.top = `${y}px`;
+  // used translate for better performance
+  // translate only require composition phase
+  vertical.style.transform = `translateX(${x}px)`;
+  horizontal.style.transform = `translateY(${y}px)`;
+  target.style.transform = `translate(${x - targetHalfWidth}px, ${
+    y - targetHalfHeight
+  }px)`;
+  tag.style.transform = `translate(${x}px, ${y}px)`;
 
   tag.innerHTML = `${x}, ${y}`;
 });
